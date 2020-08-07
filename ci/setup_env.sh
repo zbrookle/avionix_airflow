c531b5b8d2b98bb4d48d0ebc6423f9b9ff566b25
zbrookle <zachb1996@yahoo.com>
2020-08-07 18:12:04 -0400

    BUG: Change version of minikube

--- ci/setup_env.sh
+++ ci/setup_env.sh
@@ -8,7 +8,7 @@ sudo apt update
 sudo apt install docker-ce
 
 # Install minikube
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v1.11.0/minikube-linux-amd64 \
   && chmod +x minikube
 sudo mkdir -p /usr/local/bin/
 sudo install minikube /usr/local/bin/
