ec7f9321ad523f15b5ca4ee4b99d1df5399dfcb2
zbrookle <zachb1996@yahoo.com>
2020-08-11 19:50:48 -0400

    BUG: Run function not as default argument

--- avionix_airflow/host_settings.py
+++ avionix_airflow/host_settings.py
@@ -2,14 +2,16 @@ from python_hosts import Hosts, HostsEntry
 
 from avionix_airflow.kubernetes.airflow import AirflowOptions
 from avionix_airflow.kubernetes.utils import get_minikube_ip
-
+from typing import Optional
 
 def add_host(
     airflow_options: AirflowOptions,
     entry_type: str = "ipv4",
-    address: str = get_minikube_ip(),
+    address: Optional[str] = None,
     force: bool = False,
 ):
+    if address is None:
+        address = get_minikube_ip()
     my_hosts = Hosts()
     my_hosts.add(
         [HostsEntry(entry_type, address=address, names=[airflow_options.domain_name])],
