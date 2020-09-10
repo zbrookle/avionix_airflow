58ddedf07aa71f25cf382007ec7817303d8c19bc
zbrookle <zachb1996@yahoo.com>
2020-09-10 14:48:24 -0400

    DOC: Update readme to go on PyPi

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from distutils.core import setup
-import os
 from pathlib import Path
 
 from setuptools import find_packages
@@ -9,11 +8,9 @@ import versioneer
 CODE_DIRECTORY = Path(__file__).parent
 
 
-def read_file(filename):
+def read_file(file_path: Path):
     """Source the contents of a file"""
-    with open(
-        os.path.join(os.path.dirname(__file__), filename), encoding="utf-8"
-    ) as file:
+    with open(str(file_path), encoding="utf-8") as file:
         return file.read()
 
 
@@ -22,13 +19,13 @@ setup(
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     packages=find_packages(),
-    long_description="Coming soon...",
+    long_description=read_file(CODE_DIRECTORY / "README.rst"),
     maintainer="Zach Brookler",
     maintainer_email="zachb1996@yahoo.com",
     description="A package for deploying airflow to kubernetes with included "
     "monitoring (grafana and elasticsearch integration)",
     python_requires=">=3.6.1",
-    install_requires=[read_file("requirements.txt").split("\n")],
+    install_requires=[read_file(CODE_DIRECTORY / "requirements.txt").split("\n")],
     project_urls={
         "Source Code": "https://github.com/zbrookle/avionix_airflow",
         "Documentation": "https://github.com/zbrookle/avionix_airflow",
@@ -56,6 +53,6 @@ setup(
         "Typing :: Typed",
         "Operating System :: OS Independent",
     ],
-    long_description_content_type="text/markdown",
+    long_description_content_type="text/x-rst",
     include_package_data=True,
 )
